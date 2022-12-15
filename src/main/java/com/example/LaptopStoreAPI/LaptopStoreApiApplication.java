e6ea566a55ed56cc9ad7a22b68ad90f3932298f6
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-15 11:48:58 +0700

    Init project

---
+++ src/main/java/com/example/LaptopStoreAPI/LaptopStoreApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.LaptopStoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopStoreApiApplication {public static void main(String[] args) {
+		SpringApplication.run(LaptopStoreApiApplication.class, args);
+	}
+
+}

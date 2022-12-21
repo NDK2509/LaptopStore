ce791661b1296406c0e8a610b96153453803e221
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-22 02:50:47 +0700

    fix: exclude spring security

--- src/main/java/com/example/LaptopStoreAPI/LaptopStoreApiApplication.java
+++ src/main/java/com/example/LaptopStoreAPI/LaptopStoreApiApplication.java
@@ -2,10 +2,14 @@ package com.example.LaptopStoreAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class LaptopStoreApiApplication {public static void main(String[] args) {
-		SpringApplication.run(LaptopStoreApiApplication.class, args);
-	}
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@RestController
+public class LaptopStoreApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopStoreApiApplication.class, args);
+    }
 
 }

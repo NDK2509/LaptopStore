025ebd23ae4299a6be888e834f67d0075c66d545
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-22 02:52:27 +0700

    chore: add jwt and mongodb libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,11 @@ repositories {
 }
 
 dependencies {
+	/// jwt
+	implementation ("com.auth0:java-jwt:4.2.1")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.0.0")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")

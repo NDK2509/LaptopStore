845cff01ba0dea592d0a27729b94c098f402e9ca
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-22 02:48:28 +0700

    feat: add config

---
+++ src/main/java/com/example/LaptopStoreAPI/configs/EnvConfig.java
@@ -0,0 +1,20 @@
+package com.example.LaptopStoreAPI.configs;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@Getter
+public class EnvConfig {
+    @Value("${secret-key.accessToken}")
+    private String accessToken;
+    @Value("${gmt-offset}")
+    private int GMTOffset;
+    @Value("${expired-time.access-token}")
+    private int accessTokenExpiredTime;
+    @Value("${expired-time.refresh-token}")
+    private int refreshTokenExpiredTime;
+}

382dee17f38479167bea25507c72ee1230ce4b18
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-27 05:23:52 +0700

    feat: verify access on AuthFilter

--- src/main/java/com/example/LaptopStoreAPI/middlewares/AuthFilter.java
+++ src/main/java/com/example/LaptopStoreAPI/middlewares/AuthFilter.java
@@ -2,24 +2,24 @@ package com.example.LaptopStoreAPI.middlewares;
 
 import com.example.LaptopStoreAPI.configs.EnvConfig;
 import com.example.LaptopStoreAPI.services.ITokenHandler;
-import com.example.LaptopStoreAPI.services.IUserService;
 import com.example.LaptopStoreAPI.utils.JWTUtils;
 import com.example.LaptopStoreAPI.utils.RequestUtils;
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.util.Optional;
+
 @WebFilter(urlPatterns = {"/api/v1/*"})
+@AllArgsConstructor
 public class AuthFilter extends HttpFilter {
+    private ITokenHandler jwtHandler;
+    private EnvConfig env;
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         var token =
@@ -31,6 +31,12 @@ public class AuthFilter extends HttpFilter {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Required access token!");
             return;
         }
-        chain.doFilter(request, response);
+        if (jwtHandler.verifyToken(token, env.getAccessTokenSecret())) {
+            chain.doFilter(request, response);
+        }
+        else {
+            response.setContentType("application/json");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access token invalid!");
+        }
     }
 }

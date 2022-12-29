8e6dadb6960fa37067520f1e9a90aa3b1cf58547
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-29 22:12:49 +0700

    feat: add create, update. delete features

--- src/main/java/com/example/LaptopStoreAPI/controllers/ProductApiController.java
+++ src/main/java/com/example/LaptopStoreAPI/controllers/ProductApiController.java
@@ -4,7 +4,6 @@ import com.example.LaptopStoreAPI.models.Product;
 import com.example.LaptopStoreAPI.resposes.ApiResponse;
 import com.example.LaptopStoreAPI.resposes.ProductApiResponse;
 import com.example.LaptopStoreAPI.services.IProductService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,39 +13,55 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/products")
 @CrossOrigin("*")
 @AllArgsConstructor
-public class ProductApiController implements ApiController<ApiResponse, Product> {
+public class ProductApiController implements ApiController<ApiResponse<?>, Product> {
     protected IProductService productService;
 
     @Override
     @GetMapping("")
-    public ResponseEntity<ApiResponse> getAll(HttpServletRequest request) {
+    public ResponseEntity<ApiResponse<?>> getAll() {
 
         var productList = productService.findAll();
         if (productList.size() == 0) {
             return new ResponseEntity<>(new ApiResponse<>(null, "No data!"), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(new ApiResponse<>(productList, "Get data successfully!"), HttpStatus.OK);
+        return new ResponseEntity<>(new ProductApiResponse(productList, "Get data successfully!"), HttpStatus.OK);
     }
 
     @Override
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getOneById(HttpServletRequest request, @PathVariable("id") String id) {
+    public ResponseEntity<ApiResponse<?>> getById(@PathVariable("id") String id) {
         var product = productService.findById(id);
         if (product == null) {
-            return new ResponseEntity<>(new ProductApiResponse(null, "Product is not found!"), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(new ApiResponse<>(null, "Product is not found!"), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(new ProductApiResponse(product, "Get product successfully!"), HttpStatus.OK);
+        return new ResponseEntity<>(new ApiResponse<>(product, "Get product successfully!"), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<ApiResponse> save(HttpServletRequest request, Product model) {
-        return null;
+    @PostMapping("")
+    public ResponseEntity<ApiResponse<?>> create(@RequestBody Product product) {
+        var createdProduct = productService.create(product);
+        return new ResponseEntity<>(new ApiResponse<>(createdProduct, "Create new product successfully!"), HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<ApiResponse> delete(HttpServletRequest request, String id) {
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<?>> update(@PathVariable("id") String id, Product product) {
+        if (productService.exitsById(id)) {
+            product.setId(id);
+            var updatedProduct = productService.update(product);
+            return new ResponseEntity<>(new ApiResponse<>(updatedProduct, "Update successfully!"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ApiResponse<>(null, "Product not exists!"), HttpStatus.NOT_FOUND);
     }
 
-
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<?>> delete(@PathVariable("id") String id) {
+        if (productService.exitsById(id)) {
+            var updatedProduct = productService.delete(id);
+            return new ResponseEntity<>(new ApiResponse<>(updatedProduct, "Delete successfully!"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ApiResponse<>(null, "Product not exists!"), HttpStatus.NOT_FOUND);
+    }
 }

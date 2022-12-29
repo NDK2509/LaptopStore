933a4f5260861fc057f6f0527080ac1787b2ac6b
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-29 22:14:08 +0700

    feat: add validation for Product and User models

--- src/main/java/com/example/LaptopStoreAPI/models/Product.java
+++ src/main/java/com/example/LaptopStoreAPI/models/Product.java
@@ -1,15 +1,34 @@
 package com.example.LaptopStoreAPI.models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Data
 @Document(collection = "Products")
 public class Product {
     @Id
     private String id;
+    @NotEmpty(message = "Name is required!")
+    @NotNull(message = "Name is required!")
     private String name;
+    @NotNull(message = "Price is required!")
+    @Positive(message = "Price must be greater than 0!")
     private float price;
+    @NotNull(message = "Created date is required!")
     private long createdDate;
+
+    public Product(String name, float price) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.price = price;
+        this.createdDate = new Date().getTime();
+    }
 }
--- src/main/java/com/example/LaptopStoreAPI/models/User.java
+++ src/main/java/com/example/LaptopStoreAPI/models/User.java
@@ -1,5 +1,6 @@
 package com.example.LaptopStoreAPI.models;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,8 +14,15 @@ import java.util.UUID;
 public class User {
     @Id
     private String userId;
+    @NotNull(message = "Username is required!")
+    @NotEmpty(message = "Username is required!")
     private String username;
+    @NotNull(message = "Password is required!")
+    @NotEmpty(message = "Password is required!")
+    @Size(min = 4, message = "Password must has more than or equals 4 characters!")
     private String password;
+    @NotEmpty(message = "Password is required!")
+    @Email(message = "Invalid email!")
     private String email;
 
     public User(String username, String password, String email) {
